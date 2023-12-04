a77e929689bc0098da7c7c9b9ca7a4040bfaff0d
VLstack <github@vlstack.fr>
2023-12-04 14:16:44 +0100

    rename local colors expose colors to kit

--- Sources/VLBrandKit/VLBrandKit.swift
+++ Sources/VLBrandKit/VLBrandKit.swift
@@ -1,46 +1,51 @@
-import Foundation
 import SwiftUI
 
 public 
 enum Images
 {
- public
- static let logo = Image("VLstack-logo", bundle: .module)
+ public static let logo = Image("logo", bundle: .module)
 }
 
-//internal
-//class CurrentBundleFinder {}
-//
-//internal
-//extension Foundation.Bundle
-//{
-// static var localModule: Bundle =
-// {
-//  let nameIOs = "LocalPackages_VLBrandKit"
-//  let nameMacOs = "PackageName_VLBrandKit"
-//  let candidates = [
-//   Bundle.main.resourceURL,
-//   Bundle(for: CurrentBundleFinder.self).resourceURL,
-//   Bundle.main.bundleURL,
-//   Bundle(for: CurrentBundleFinder.self).resourceURL?.deletingLastPathComponent(),
-//   Bundle(for: CurrentBundleFinder.self).resourceURL?.deletingLastPathComponent().deletingLastPathComponent(),
-//   Bundle(for: CurrentBundleFinder.self).resourceURL?.deletingLastPathComponent().deletingLastPathComponent().deletingLastPathComponent()
-//  ]
-//  
-//  for candidate in candidates
-//  {
-//   let pathIOs = candidate?.appendingPathComponent(nameIOs + ".bundle")
-//   let pathMacOs = candidate?.appendingPathComponent(nameMacOs + ".bundle")
-//   if let bundle = pathIOs.flatMap(Bundle.init(url:))
-//   {
-//    return bundle
-//   }
-//   if let bundle = pathMacOs.flatMap(Bundle.init(url:))
-//   {
-//    return bundle
-//   }
-//  }
-//  
-//  fatalError("unable to find bundle")
-// }()
-//}
+public
+enum Colors
+{
+ public static let primary100 = Color("primary-100", bundle: .module)
+ public static let primary200 = Color("primary-200", bundle: .module)
+ public static let primary300 = Color("primary-300", bundle: .module)
+ public static let primary400 = Color("primary-400", bundle: .module)
+ public static let primary500 = Color("primary-500", bundle: .module)
+ public static let primary600 = Color("primary-600", bundle: .module)
+ public static let primary700 = Color("primary-700", bundle: .module)
+ public static let primary800 = Color("primary-800", bundle: .module)
+ public static let primary900 = Color("primary-900", bundle: .module)
+ 
+ public static let primary100On = Color("primary-100-on", bundle: .module)
+ public static let primary200On = Color("primary-200-on", bundle: .module)
+ public static let primary300On = Color("primary-300-on", bundle: .module)
+ public static let primary400On = Color("primary-400-on", bundle: .module)
+ public static let primary500On = Color("primary-500-on", bundle: .module)
+ public static let primary600On = Color("primary-600-on", bundle: .module)
+ public static let primary700On = Color("primary-700-on", bundle: .module)
+ public static let primary800On = Color("primary-800-on", bundle: .module)
+ public static let primary900On = Color("primary-900-on", bundle: .module)
+ 
+ public static let secondary100 = Color("secondary-100", bundle: .module)
+ public static let secondary200 = Color("secondary-200", bundle: .module)
+ public static let secondary300 = Color("secondary-300", bundle: .module)
+ public static let secondary400 = Color("secondary-400", bundle: .module)
+ public static let secondary500 = Color("secondary-500", bundle: .module)
+ public static let secondary600 = Color("secondary-600", bundle: .module)
+ public static let secondary700 = Color("secondary-700", bundle: .module)
+ public static let secondary800 = Color("secondary-800", bundle: .module)
+ public static let secondary900 = Color("secondary-900", bundle: .module)
+ 
+ public static let secondary100On = Color("secondary-100-on", bundle: .module)
+ public static let secondary200On = Color("secondary-200-on", bundle: .module)
+ public static let secondary300On = Color("secondary-300-on", bundle: .module)
+ public static let secondary400On = Color("secondary-400-on", bundle: .module)
+ public static let secondary500On = Color("secondary-500-on", bundle: .module)
+ public static let secondary600On = Color("secondary-600-on", bundle: .module)
+ public static let secondary700On = Color("secondary-700-on", bundle: .module)
+ public static let secondary800On = Color("secondary-800-on", bundle: .module)
+ public static let secondary900On = Color("secondary-900-on", bundle: .module)
+}
