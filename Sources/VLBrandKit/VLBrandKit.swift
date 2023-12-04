8505ad9da5e3cc30933058f16cab355e9f1e7db1
VLstack <github@vlstack.fr>
2023-12-04 13:53:02 +0100

    next bundle test

--- Sources/VLBrandKit/VLBrandKit.swift
+++ Sources/VLBrandKit/VLBrandKit.swift
@@ -16,23 +16,31 @@ extension Foundation.Bundle
 {
  static var localModule: Bundle =
  {
-  let bundleName = "LocalPackages_VLBrandKit"
+  let nameIOs = "LocalPackages_VLBrandKit"
+  let nameMacOs = "PackageName_VLBrandKit"
   let candidates = [
    Bundle.main.resourceURL,
    Bundle(for: CurrentBundleFinder.self).resourceURL,
    Bundle.main.bundleURL,
-   Bundle(for: CurrentBundleFinder.self).resourceURL?.deletingLastPathComponent().deletingLastPathComponent()
+   Bundle(for: CurrentBundleFinder.self).resourceURL?.deletingLastPathComponent(),
+   Bundle(for: CurrentBundleFinder.self).resourceURL?.deletingLastPathComponent().deletingLastPathComponent(),
+   Bundle(for: CurrentBundleFinder.self).resourceURL?.deletingLastPathComponent().deletingLastPathComponent().deletingLastPathComponent()
   ]
   
   for candidate in candidates
   {
-   let path = candidate?.appendingPathComponent(bundleName + ".bundle")
-   if let bundle = path.flatMap(Bundle.init(url:))
+   let pathIOs = candidate?.appendingPathComponent(nameIOs + ".bundle")
+   let pathMacOs = candidate?.appendingPathComponent(nameMacOs + ".bundle")
+   if let bundle = pathIOs.flatMap(Bundle.init(url:))
+   {
+    return bundle
+   }
+   if let bundle = pathMacOs.flatMap(Bundle.init(url:))
    {
     return bundle
    }
   }
   
-  fatalError("unable to find bundle named \(bundleName)")
+  fatalError("unable to find bundle")
  }()
 }
