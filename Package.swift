2b1c7ec1dbd5dc3c9ede7d8ab7cda80b5e1a1787
VLstack <github@vlstack.fr>
2025-05-07 09:29:25 +0200

    comply with VLstackNamespace 1.2.0

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(name: "VLBrandKit",
                       ],
                       dependencies:
                       [
-                       .package(url: "https://github.com/VLstack/VLstackNamespace", from: "1.1.0")
+                       .package(url: "https://github.com/VLstack/VLstackNamespace", from: "1.2.0")
                       ],
                       targets:
                       [
